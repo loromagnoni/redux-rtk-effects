6ae0fb43fbdde80fe9d984cb63ae28ef50b013e7
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2023-06-04 21:09:38 +0200

    test: createEnvironment tests

---
+++ test/createEnvironment.test.ts
@@ -0,0 +1,18 @@
+import { assertType, expect, expectTypeOf, test } from 'vitest';
+import { createEnvironment } from '../src/createEnvironment';
+
+type CustomRepository = {};
+
+type CustomEnvironment = {
+  repository: CustomRepository;
+};
+
+test('create environment should type the result as the type argument', () => {
+  expectTypeOf(createEnvironment<CustomEnvironment>()).toEqualTypeOf<CustomEnvironment>();
+});
+
+test('create environment should return just an empty object', () => {
+  const environment = createEnvironment<CustomEnvironment>();
+  assertType<CustomEnvironment>(environment);
+  expect(Object.keys(environment)).toHaveLength(0);
+});
